1268318495495908932faae8622a9dbe85b7b424
Балагуров Лев Дмитриевич <BLD.18@lore.uni-dubna.ru>
2019-02-08 15:55:10 +0300

    calculator

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxMain = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBoxMain
+            // 
+            this.textBoxMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMain.Location = new System.Drawing.Point(12, 12);
+            this.textBoxMain.Name = "textBoxMain";
+            this.textBoxMain.Size = new System.Drawing.Size(522, 27);
+            this.textBoxMain.TabIndex = 0;
+            this.textBoxMain.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextField_KeyPress);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(546, 306);
+            this.ClientSize = new System.Drawing.Size(546, 245);
+            this.Controls.Add(this.textBoxMain);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxMain;
     }
 }
 
--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -12,9 +12,92 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        private Dictionary<char, Func<double, double, double>> operandsMap = new Dictionary<char, Func<double, double, double>>
+        {
+            {'+', (left, right) => left + right},
+            {'-', (left, right) => left - right},
+            {'/', (left, right) => left / right},
+            {'*', (left, right) => left * right},
+        };
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();           
+        }
+
+        private string process(string s) {
+            try
+            {
+                double res = parseAndCalc(s);
+
+                return Math.Round(res, 3).ToString();
+            }
+            catch(Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        private double parseAndCalc(string s)
+        {
+            OperatingTuple? tuple = parse(s);
+
+            if (tuple == null)
+                throw new Exception("Wrong input");
+
+            return calc(tuple.GetValueOrDefault());
+        }
+
+        private double calc(OperatingTuple tuple)
+        {
+            return operandsMap[tuple.operand](tuple.leftArg, tuple.rightArg);
+        }
+
+        private OperatingTuple? parse(string s)
+        {
+            string[] arr = s.Split(' ');
+
+            if (arr.Length != 3)
+                return null;
+
+            string leftArgStr = arr[0];
+            string operStr = arr[1];
+            string rigthArgStr = arr[2];
+
+            double leftArg, rightArg;
+
+            if (!(double.TryParse(leftArgStr, out leftArg) && double.TryParse(rigthArgStr, out rightArg)))
+                return null;
+
+            if (!operandsMap.ContainsKey(operStr[0]))
+                return null;
+
+            return new OperatingTuple(leftArg, rightArg, operStr[0]);
+        }
+
+        private void TextField_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                string val = textBoxMain.Text;
+                textBoxMain.Text = process(val);
+
+                textBoxMain.SelectionStart = textBoxMain.Text.Length;
+                textBoxMain.SelectionLength = 0;
+            }
+        }
+    }
+
+    struct OperatingTuple
+    {
+        public double leftArg, rightArg;
+        public char operand;
+
+        public OperatingTuple(double leftArg, double rightArg, char operand)
+        {
+            this.leftArg = leftArg;
+            this.rightArg = rightArg;
+            this.operand = operand;
         }
     }
 }
